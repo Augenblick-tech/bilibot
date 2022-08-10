b331a3560d63209f331372355a6d1cb7af1d61a0
lonzzi <lonzzi@qq.com>
2022-08-10 14:01:00 +0800

    style: add more columns

--- pkg/db/schema.sql
+++ pkg/db/schema.sql
@@ -9,18 +9,24 @@ CREATE TABLE `users` (
 
 CREATE TABLE `bots` (
   `uid` INTEGER NOT NULL PRIMARY KEY,
+  `name` VARCHAR(255) NOT NULL,
+  `face` TEXT,
   `user_id` INTEGER NOT NULL,
   FOREIGN KEY (`user_id`) REFERENCES users (`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `uploaders` (
   `uid` INTEGER NOT NULL PRIMARY KEY,
+  `name` VARCHAR(255) NOT NULL,
+  `face` TEXT,
   `bot_id` INTEGER NOT NULL,
   FOREIGN KEY (`bot_id`) REFERENCES bots (`uid`) ON DELETE CASCADE
 );
 
 CREATE TABLE `dynamics` (
   `dynamic_id` TEXT NOT NULL PRIMARY KEY,
+  `pub_ts` INTEGER NOT NULL,
+  `content` TEXT NOT NULL,
   `up_id` INTEGER NOT NULL,
   FOREIGN KEY (`up_id`) REFERENCES uploaders (`uid`) ON DELETE CASCADE
 );
